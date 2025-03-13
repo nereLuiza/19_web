8ad70198e08aa6bcf32983b524f1dfc224b77714
nereLuiza <151404346+nereLuiza@users.noreply.github.com>
2025-03-13 12:13:24 +0000

    CRUD

--- js/home.js
+++ js/home.js
@@ -1,23 +1,68 @@
-import { verificarAutenticacao } from './autorizar.js';
+const urlBase = "https://13-web.vercel.app";
 
-// Função anônima auto-executável assíncrona.
-// O ()() garante que ela seja executada imediatamente ao carregar o script
-(async () => {
-  // Chama a função 'verificarAutenticacao' de forma assíncrona e espera seu resultado.
-  const autenticado = await verificarAutenticacao();
+const tabelaCorpo = document.getElementById("tabela-usuarios");
+tabelaCorpo.innerHTML = 'Aguarde...';
 
-  // Obtém o elemento HTML com o ID 'loading-overlay' (o overlay de carregamento da página)
-  const overlay = document.getElementById('loading-overlay');
+try {
+  const endpoint = '/usuario';
+  const urlFinal = urlBase + endpoint;
+  const response = await fetch(urlFinal);
 
-  // Obtém o elemento HTML com o ID 'conteudo-protegido' (o conteúdo restrito que deve ser exibido após autenticação)
-  const conteudo = document.getElementById('conteudo-protegido');
+  if (!response.ok) {
+    throw new Error("Erro na requisição: " + response.status);
+  }
 
-  // Verifica se o usuário foi autenticado com sucesso
-  if (autenticado) {
-    // Remove o elemento 'overlay' do DOM, ocultando o texto "Carregando..." da tela
-    overlay.remove();
+  const data = await response.json();
+  tabelaCorpo.innerHTML = '';
 
-    // Altera o estilo do elemento 'conteudo' para 'block', tornando o conteúdo visível
-    conteudo.style.display = 'block';
-  } 
-})();
+  // Loop para preencher a tabela
+  data.forEach(usuario => {
+    const linha = document.createElement("tr");
+    linha.innerHTML = `
+                <td>${usuario.id}</td>
+                <td>${usuario.nome}</td>
+                <td>${usuario.email}</td>
+                <td>${usuario.senha}</td>
+                <td class="acoes">
+                  <a class="botaoVer" href="usuario.html?id=${usuario.id}">Ver</a> | 
+                  <a class="botaoAlterar" href="alterar-usuario.html?id=${usuario.id}">Alterar</a> | 
+                  <a class="botaoExcluir" href="${usuario.id}">Excluir</a>
+                </td>
+            `;
+    tabelaCorpo.appendChild(linha);
+  });
+} catch (error) {
+  console.error("Erro:", error);
+}
+
+
+tabelaCorpo.addEventListener('click', acao);
+
+function acao(e) {
+  if (e.target.classList.contains("botaoExcluir")) {
+    e.preventDefault();
+    const id = e.target.getAttribute("href");
+    excluirUsuario(id);
+  }
+}
+
+async function excluirUsuario(id) {
+  try {
+    const endpoint = `/usuario/${id}`;
+    const urlFinal = urlBase + endpoint;
+    const response = await fetch(urlFinal, {
+      method: 'DELETE'
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    
+    alert('Usuário excluido com sucesso!');
+
+  } catch (error) {
+    console.error(error);
+    alert('Usuário excluido não foi excluido!');
+  }
+  window.location.href = 'home.html';
+}
